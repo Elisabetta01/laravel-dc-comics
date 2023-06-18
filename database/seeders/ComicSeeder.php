db9dabfdcb1201c39ae8b04e19add7bd168a5f8e
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-06-18 17:39:46 +0200

    seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -14,6 +16,18 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $arrayComics = config('comics');
+
+        foreach($arrayComics as $element){
+            $newComic = new Comic();
+            $newComic->title = $element['title'];
+            $newComic->description = $element['description'];
+            $newComic->thumb = $element['thumb'];
+            $newComic->price = $element['price'];
+            $newComic->series = $element['series'];
+            $newComic->sale_date = $element['sale_date'];
+            $newComic->type = $element['type'];
+            $newComic->save();
+        }
     }
 }
